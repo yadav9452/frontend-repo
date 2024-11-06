8e4d32b5be571fa44907e8a0a3a3415cb6e377c5
yadav9452 <yrahul9452@gmail.com>
2024-11-06 23:39:15 +0530

    function

--- frontend/js/section10:functions/functions.js
+++ frontend/js/section10:functions/functions.js
@@ -122,7 +122,7 @@ function count() {
 */
 
 // HIGHER ORDER FUNCTIONS
-
+/*
 const oneWord = function (str) {
   return str.replace(/ /g, " ").toLowerCase();
 };
@@ -164,5 +164,114 @@ const greetArr =
   (name = "RAHUL") =>
     console.log(`${greeting} ${name}`);
 greetArr("HELLO")("BRUCE");
+*/
 
-// THE CALL AND APPLY METHODS
+// THE CALL, APPLY AND BIND METHODS
+//  changing the referenc of this keyword or manipulating the this keywords
+// manually setting this keyword
+
+/*
+const lufthansa = {
+  airline: "Lufthansa",
+  iatacode: "LH",
+  booking: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightNum}`
+    );
+    this.booking.push({ flight: `${this.iatacode}${flightNum}`, name });
+  },
+};
+lufthansa.book(239, "Deepali Yadav");
+lufthansa.book(635, "Roshan Adhikari");
+console.log(lufthansa.booking);
+// this is the example where a method is used for one object
+//  now lets use the function from object 1 in the other object.
+// assigning book method to a variable
+const book = lufthansa.book;
+
+const eurowings = {
+  airline: "EuroWings",
+  iatacode: "EW",
+  booking: [],
+};
+// method.call(targetObject, all_arguments);
+book.call(eurowings, 23, "Divya Yadav");
+book.call(lufthansa, 439, "Babu Yadav");
+const swiss = {
+  airline: "swiss Air Index",
+  iatacode: "LX",
+  booking: [],
+};
+book.call(swiss, 583, "Pihu Yadav");
+console.log(lufthansa);
+console.log(eurowings);
+console.log(swiss);
+
+// APPLY Methods
+// it does the same thing but it receive the array of the argument instead of list of arguments
+const flightData = [583, "Ramesh Yadav"];
+book.apply(swiss, flightData);
+
+// in modern js this methods is rarly used since we can use the call method with spread operator
+book.call(swiss, ...flightData);
+console.log(swiss);
+// the above method will work in the same way.
+
+//BIND METHODS
+// 1. it sets the object method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+console.log(bookEW);
+bookEW(23, "Rahul");
+bookLH(583, "Suzaen");
+
+//  we can also give intial argument with bind methods
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("sarthak");
+bookEW23("akshay");
+
+// 2. With EVENT LISTENER how to use the bind method
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+document
+  .querySelector(".buy-plane")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// 3. partial application on bind methods
+// preset parameters
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT= value => value + value * 0.23;
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// IIFE IMMEDIATELY INVOKED FUNCTION EXPRESSION are not used nowdays in modrn js
+// since due to let and const the data encapsulation
+// 1. first way
+const runOnce = function () {
+  console.log("this will never run");
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log("this will never run again");
+})();
+
+// for arrow function
+(() => console.log("this arrow function will never run again"))();
+// Why these pattern created for IIFE
+*/
+
+// CLOSURES: THIS IS THE SITUATION IN CODE 
