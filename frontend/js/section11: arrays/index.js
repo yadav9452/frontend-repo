1e27feffe111161bb4bd9b90f043f1fd336447e1
rahul yadav <yrahul9452@gmail.com>
2024-11-09 20:09:32 +0530

    array section added

--- frontend/js/section11: arrays/index.js
+++ frontend/js/section11: arrays/index.js
@@ -11,3 +11,39 @@ console.log(arr);
 console.log(arr.slice()); // (6) [1, 2, 3, 4, 5, 6]
 console.log(arr.slice(0)); // (6) [1, 2, 3, 4, 5, 6]
 console.log(arr.slice());
+// refer the
+// slice
+// splice
+// concat
+// reverse
+// join
+// at method in string and array
+// foreach methods on arrays: the order matters - forEach((current_element , index , array))
+// high order funcitons
+// for each on maps and sets
+
+// Map, filter and reduce
+// try with the examples
+// reduce methods: return one value (acc, curr, i , arr)
+// chaining the methods
+// find methods
+// find vs filter
+// findIndex mehtods:
+// some methods
+// every methods
+// includes
+// flat and flatmaps(only one level deep) methods:- by default one level deep
+// array.sort()
+// more ways to create and fill the arrays.
+// 1.
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// 2.
+console.log(new Array(1, 2, 3, 4, 5, 6, 7, 8, 9));
+// 3.
+const x = new Array(7);
+// map doesnot work here
+x.fill(1);
+// this method will fill the whole array with the same value
+console.log(x);
+
+// which array method to use
